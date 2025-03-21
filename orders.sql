759d4b72db19e60d77e3c13933adbaa7a2534ed1
M95 <tommynara@gmail.com>
2025-03-21 12:06:35 +0900

    Update orders.sql

--- orders.sql
+++ orders.sql
@@ -1,3 +1,8 @@
+use shop_db;
+-- customers 테이블 생성 
+CREATE TABLE customers ( customer_id int primary key, customer_name VARCHAR(100), total_purchase DECIMAL(10, 2) ); 
+-- 데이터 삽입 
+INSERT INTO customers (customer_id, customer_name, total_purchase) VALUES (1, 'Alice', 12000), (2, 'Bob', 8000), (3, 'Charlie', 4500), (4, 'David', 6000), (5, 'Eva', 15000);
 -- 주문 테이블
 CREATE TABLE orders (
     order_id INT PRIMARY KEY,
@@ -17,7 +22,5 @@ INSERT INTO orders (order_id, customer_id, order_date, total_amount, payment_met
 (1004, 1, '2023-02-20 09:30:00', 65000.00, '간편결제', 'completed'),
 (1005, 4, '2023-03-08 13:20:00', 120000.00, '신용카드', 'completed'),
 (1006, 5, '2023-03-15 11:10:00', 35000.00, '간편결제', 'completed'),
-(1007, 6, '2023-04-02 15:45:00', 150000.00, '신용카드', 'completed'),
 (1008, 2, '2023-04-18 14:30:00', 120000.00, '신용카드', 'completed'),
-(1009, 3, '2023-05-05 10:15:00', 25000.00, '무통장입금', 'cancelled'),
-(1010, 7, '2023-05-20 16:40:00', 850000.00, '신용카드', 'completed');
+(1009, 3, '2023-05-05 10:15:00', 25000.00, '무통장입금', 'cancelled')
